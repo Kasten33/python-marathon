2c250e812bcc8d385de5cd824802486b7f6cb28b
Kasten33 <kadenhurley5@gmail.com>
2024-10-08 16:06:38 -0600

    ISS API project is done

---
+++ Module 4/ISS/main.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+Email = "hurlrykaden@gmail.com"
+Password = "kh205114"
+
+lat = 40.71
+lon = -111.89
+
+parameters = {
+    "lat": lat,
+    "lng": lon,
+    "formatted": 0
+}
+def ISS():
+
+
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    ISSLat = float(data["iss_position"]["latitude"])
+    ISSLon = float(data["iss_position"]["longitude"])
+
+    if lat-5 <= ISSLat <= lat+5 and lon-5 <= ISSLon <= -lon+5:
+        return True
+
+def night():
+    response2 = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response2.raise_for_status() 
+    data = response2.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+
+
+
+
+
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+
+while True:
+    time.sleep(300)
+
+    if ISS() and night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(Email, Password)
+        connection.sendmail(
+            from_addr=Email, 
+            to_addrs=Email,
+            Subject="Look Up",
+            msg="The ISS is above you in the sky"
+           )
+        connection.close() 
