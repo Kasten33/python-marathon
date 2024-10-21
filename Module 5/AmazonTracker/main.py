ac0bf5bc19bd568f75b2c8b82d8edfe87872a59f
Kasten33 <hurlrykaden@gmail.com>
2024-10-21 10:20:08 -0600

    the datetime functionallity is a little scuffed

--- Module 5/AmazonTracker/main.py
+++ Module 5/AmazonTracker/main.py
@@ -1,14 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
+import smtplib
+import os
+from datetime import datetime
+from dotenv import load_dotenv
 
-response = requests.get("https://appbrewery.github.io/instant_pot/")
+load_dotenv()
+
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+
+response = requests.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1", headers={"Accept-Language":"en-US"})
 webPage = response.text
 
 soup = BeautifulSoup(webPage, "html.parser")
 price = soup.find(name="span", class_="a-price-whole").getText()
 # fraction = soup.find(name="span", class_="a-price-fraction").getText()
+def StartTracking():
+    while True:
+        now = datetime.now()
+        if now.hour == 9 and now.minute == 0:
+            if price <= "100":
+                connection = smtplib.SMTP("smtp.gmail.com")
+                connection.starttls()
+                connection.login(email, password)
+                connection.sendmail(
+                    from_addr=email, 
+                    to_addrs=email,
+                    msg=f"Subject:Price Drop\n\nThe price of the item you are tracking has dropped to {price}.\n\n"
+                )
+                connection.close()
+            else:
+                print("Price is still too high")
+                break
+        else:
+            print("Not the right time")
+            break
 
-if price >= "100":
-    print("Buy!")
-else:
-    print("Not Yet")
+StartTracking() 
